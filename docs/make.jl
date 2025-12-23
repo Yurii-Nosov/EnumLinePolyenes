dd912332a31f09158b576ad6567984a39dad26fb
Yurii-Nosov <y.nosov.math@gmail.com>
2025-12-23 21:33:02 +0300

    Update make.jl
    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,7 +1,15 @@
-using Documenter, EnumLinePolyenes
+using Documenter
+using DocumenterCitations 
+using EnumLinePolyenes
+
+bib = CitationBibliography(joinpath(
+    @__DIR__, "src", "refs.bib");
+    style=:numeric
+)
 
 makedocs(sitename="EnumLinePolyenes.jl",
     modules=[EnumLinePolyenes],
+    format=Documenter.HTML(),
     pages=[
         "Home" => "index.md",
         "Generation of binary codes of molecular graphs" => "Generation.md",
@@ -9,7 +17,10 @@ makedocs(sitename="EnumLinePolyenes.jl",
         "Distribution of graphs by symmetry groups" => "SymmetryGroups.md",
         "Molecular graphs with vertex overlays" => "Overlap.md",
         "Source code documentation" => "source.md",
-    ]
+        "bibliography.md",
+    ],
+    checkdocs = :exports,
+    plugins = [bib],
 )
 
 deploydocs(

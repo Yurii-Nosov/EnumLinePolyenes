fb026ffa313ca543c849cb14b92888cdf1a659ca
Yurii-Nosov <y.nosov.math@gmail.com>
2025-12-25 23:08:50 +0300

    Update genCodeBasicIsoConfV2.jl
     changed files
    src\genCodeBasicIsoConfV2.jl

--- src/genCodeBasicIsoConfV2.jl
+++ src/genCodeBasicIsoConfV2.jl
@@ -4,10 +4,27 @@ using ..AllSmallParts
 
 export makeAllCodeFromZEv2,genIsoCodeFromZEv2,genConTrnIsoCodeFromZEv2,genConOtrIsoCodeFromZEv2
  
+#     SECOND VERSION 
 
-#####  СОЗДАНИЕ ВСЕХ неизоморфных  РМВП-графов всех 3-х классов по кортежам свойств  длиной  q
-#####    использует функцию IntgDig, makeMinKirby, checkMolCodeZE
+#  Generation of vertex codes of non-isomorphic graphs of isomers, conformers of trans isomers 
+#   and conformers of other isomers
 
+##   Unlike the first version, here the `checkMinFromZE` function is used to select non-isomorphic graphs.
+
+"""
+    makeAllCodeFromZEv2(n::Int64)
+    Generates vertex codes for molecular graphs of order `n` for all non-isomorphic isomers.
+
+    This is done by sequentially generating edge codes, from which vertex codes are obtained 
+    using the `fromCodeZEtoBCode` function. Edge codes for non-isomorphic graphs are selected using 
+    the `checkMinFromZE` function.
+    # Returns
+    - `lstBcdIso::Vector{Vector{Int64}}`: A vector containing the vertex codes for molecular graphs of isomers;
+    - `lstBcdConTranIso::Vector{Vector{Int64}}`: A vector containing vertex codes for molecular graphs
+       of trans-isomer conformers;
+    - `lstBcdConOtrIso::Vector{Vector{Int64}}`: A vector containing the vertex codes for molecular
+       graphs of conformers of other isomers.
+"""
 function makeAllCodeFromZEv2(n::Int64)
     m = n - 3   #  m  - количество используемых ребер
     pmo = 2^m
@@ -35,8 +52,16 @@ function makeAllCodeFromZEv2(n::Int64)
     return  lstBcdIso, lstBcdConTI, lstBcdConBad
 end 
 
+"""
+    genIsoCodeFromZEv2(n::Int64)
+    Generates vertex codes for molecular graphs of order `n` for all non-isomorphic isomers.
 
-
+    This is done by sequentially generating edge codes, from which vertex codes are obtained 
+    using the `fromCodeZEtoBCode` function. Edge codes for non-isomorphic graphs are selected using 
+    the `checkMinFromZE` function.
+    # Returns
+    `lstBcdIso::Vector{Vector{Int64}}`: A vector containing the vertex codes for molecular graphs of isomers. 
+"""
 function genIsoCodeFromZEv2(n::Int64)
     m = n - 3   #  m  - количество используемых ребер
     pmo = 2^m
@@ -56,8 +81,19 @@ function genIsoCodeFromZEv2(n::Int64)
     return  lstBcdIso
 end 
 
+"""
+    genConTrnIsoCodeFromZEv2(n::Int64)
+    Generates vertex codes for molecular graphs of order n for all non-isomorphic conformers
+    of  trans-isomer.
 
-
+    This is done by sequentially generating edge codes, from which vertex codes are obtained 
+    using the `fromCodeZEtoBCode` function. Edge codes for non-isomorphic graphs are selected using 
+    the `checkMinFromZE` function.
+    # Returns
+    `lstBcdConTranIso::Vector{Vector{Int64}}`: A vector containing vertex codes for molecular graphs
+     of trans-isomer conformers. 
+    Each code is a vector of integers (Int64), where each element is either 0 or 1.
+"""
 function genConTrnIsoCodeFromZEv2(n::Int64)
     m = n - 3   #  m  - количество используемых ребер
     pmo = 2^m
@@ -78,7 +114,19 @@ function genConTrnIsoCodeFromZEv2(n::Int64)
     return  lstBcdConTranIso
 end 
 
+"""
+    genConOtrIsoCodeFromZEv2(n::Int64)
+    Generates vertex codes for molecular graphs of order n for all non-isomorphic conformers
+    of other isomers.
 
+    This is done by sequentially generating edge codes, from which vertex codes are obtained 
+    using the `fromCodeZEtoBCode` function. Edge codes for non-isomorphic graphs are selected using 
+    the `checkMinFromZE` function.
+    # Returns
+    `lstBcdConOtrIso::Vector{Vector{Int64}}`: A vector containing the vertex codes for molecular
+     graphs of conformers of other isomers. 
+    Each code is a vector of integers (Int64), where each element is either 0 or 1.
+"""
 function genConOtrIsoCodeFromZEv2(n::Int64)
     m = n - 3   #  m  - количество используемых ребер
     pmo = 2^m
@@ -91,8 +139,7 @@ function genConOtrIsoCodeFromZEv2(n::Int64)
             cls = graphTypeFromZE(bcd)
             if cls == 3
                 push!(setBcdConOtrIso, hvct)                                          
-            end
-                                    
+            end                                    
         end
     end 
     lstBcdConOtrIso = sort(collect(setBcdConOtrIso)) 

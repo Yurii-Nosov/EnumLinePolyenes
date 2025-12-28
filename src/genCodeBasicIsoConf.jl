a398b2ddbd80af855e5031b0116c9cfc5e5c7f5a
Yurii-Nosov <y.nosov.math@gmail.com>
2025-12-28 22:27:12 +0300

    Update genCodeBasicIsoConf.jl
    Update genCodeBasicIsoConf.jl

--- src/genCodeBasicIsoConf.jl
+++ src/genCodeBasicIsoConf.jl
@@ -9,6 +9,16 @@ export genIsoCodeFromBcd, genConTrnIsoCodeFromBcd, genConOtrIsoCodeFromBcd
 #####  СОЗДАНИЕ ВСЕХ неизоморфных  РМВП-графов всех 3-х классов по кортежам свойств  длиной  n
 #####    использует функцию IntgDig, makeMinKirby, checkMolCodeZE
 
+# Functions with source edge code
+
+"""
+All functions with source edge code use:
+- the `IntgDig`](@ref) function to generate the edge code of the molecular graph;
+- the [`makeMinFromZE`](@ref) function to check the code for canonicity based on the edge code;
+- the [`graphTypeFromZE`](@ref) function to determine the graph class based on its edge code;
+- the [`fromCodeZEtoBCode`](@ref) function to obtain the vertex code of a graph from its edge code.
+"""
+
 """
     makeAllCodeFromZE(n::Int64)
 
@@ -52,52 +62,6 @@ function makeAllCodeFromZE(n::Int64)
     return  lstBcdIso, lstBcdConTranIso, lstBcdConOtrIso
 end 
 
-
-"""
-    makeAllCodeFromBcd(n::Int64)
-
-Creates a complete lists of vertex codes for all non-isomorphic molecular graphs of 
-order `n` for isomers, conformers of trans isomers, and conformers of other isomers.
-
-The codes are created using the [`IntgDig function`](@ref). The codes are checked for 
-canonicity using the [`makeMinFromBcd`](@ref) function. Graph codes of a given class are 
-selected using the [`graphTypeFromBcd`](@ref) function.
-
-Returns    
-- `lstBcdIso::Vector{Vector{Int64}}`: A vector containing the vertex codes of the isomer 
-graphs. Each code is a vector of integers (Int64), where each element is either `0` or `1`.
-- `lstBcdConTranIso::Vector{Vector{Int64}}`: A vector containing the vertex codes of 
-the graphs of conformers of trans isomers. Each code is a vector of integers (Int64), 
-where each element is either `0` or `1`.
--`lstBcdConOtrIso::Vector{Vector{Int64}`: A vector containing the vertex codes of 
-the graphs of conformers of other isomers. Each code is a vector of integers (Int64),
-where each element is either `0` or `1`.
-"""
-function makeAllCodeFromBcd(n::Int64)
-    p = n-2   
-    pmo = 2^(p-1)
-    setBcdIso = Set()
-    setBcdConTI = Set()
-    setBcdConBad = Set()      
-    for k = 1:pmo  
-        bcd = IntgDig(k-1,p)  # n изменяется фактически от 0 до pmo - 1        
-        bcdMin = makeMinFromBcd(bcd)        
-        if bcdMin == bcd            
-            cls = graphTypeFromBcd(bcd)
-            if cls == 1
-                push!(setBcdIso, bcd)
-            elseif  cls == 2
-                push!(setBcdConTI, bcd)
-            elseif  cls == 3
-                push!(setBcdConBad, bcd)                            
-            end                        
-        end
-    end 
-    lstBcdIso = collect(setBcdIso) 
-    lstBcdConTI = collect( setBcdConTI) 
-    lstBcdConBad = collect(setBcdConBad)       
-    return  lstBcdIso, lstBcdConTI, lstBcdConBad
-end 
 
 
 
@@ -201,6 +165,53 @@ function genConOtrIsoCodeFromZE(n::Int64)
     return  lstBcdConOtrIso
 end 
 
+# Functions with source vertex code
+
+"""
+    makeAllCodeFromBcd(n::Int64)
+
+Creates a complete lists of vertex codes for all non-isomorphic molecular graphs of 
+order `n` for isomers, conformers of trans isomers, and conformers of other isomers.
+
+The codes are created using the [`IntgDig function`](@ref). The codes are checked for 
+canonicity using the [`makeMinFromBcd`](@ref) function. Graph codes of a given class are 
+selected using the [`graphTypeFromBcd`](@ref) function.
+
+Returns    
+- `lstBcdIso::Vector{Vector{Int64}}`: A vector containing the vertex codes of the isomer 
+graphs. Each code is a vector of integers (Int64), where each element is either `0` or `1`.
+- `lstBcdConTranIso::Vector{Vector{Int64}}`: A vector containing the vertex codes of 
+the graphs of conformers of trans isomers. Each code is a vector of integers (Int64), 
+where each element is either `0` or `1`.
+-`lstBcdConOtrIso::Vector{Vector{Int64}`: A vector containing the vertex codes of 
+the graphs of conformers of other isomers. Each code is a vector of integers (Int64),
+where each element is either `0` or `1`.
+"""
+function makeAllCodeFromBcd(n::Int64)
+    p = n-2   
+    pmo = 2^(p-1)
+    setBcdIso = Set()
+    setBcdConTI = Set()
+    setBcdConBad = Set()      
+    for k = 1:pmo  
+        bcd = IntgDig(k-1,p)  # n изменяется фактически от 0 до pmo - 1        
+        bcdMin = makeMinFromBcd(bcd)        
+        if bcdMin == bcd            
+            cls = graphTypeFromBcd(bcd)
+            if cls == 1
+                push!(setBcdIso, bcd)
+            elseif  cls == 2
+                push!(setBcdConTI, bcd)
+            elseif  cls == 3
+                push!(setBcdConBad, bcd)                            
+            end                        
+        end
+    end 
+    lstBcdIso = collect(setBcdIso) 
+    lstBcdConTI = collect( setBcdConTI) 
+    lstBcdConBad = collect(setBcdConBad)       
+    return  lstBcdIso, lstBcdConTI, lstBcdConBad
+end 
 
 
 """
